619a235760c04d76f587ab64d8c260c5b54cd954
Alamin Sheikh <57319298+alaminsheikh01@users.noreply.github.com>
2021-02-10 20:00:16 +0600

    value selected

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
+import React, { useState } from "react";
 import "./styles.css";
 
 export default function App() {
+  const [num, setNum] = useState();
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h1>You chose {num} value</h1>
+
+      <select
+        onChange={(e) => {
+          setNum(e.target.value);
+        }}
+      >
+        <option value="1">1</option>
+        <option value="25">25</option>
+        <option value="3">3</option>
+        <option value="4">4</option>
+      </select>
     </div>
   );
 }
